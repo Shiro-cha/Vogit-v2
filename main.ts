f6f9cc169c1f6ed7118b616bf24a040f9a9c18fa
U-TELMA\nomena30 <nomena30@LTL253VT1F.corp.telma.mg>
2026-03-15 11:08:57 +0300

    fix type error

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 import * as crypto from 'crypto';
 
-content:string= `lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+const content:string= `lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
 class Hash {
     hashValue: string;
+
+    constructor(hashValue: string) {
+        this.hashValue = hashValue;
+    }
 }
 
 class Version {
@@ -29,7 +33,7 @@ function createVersion(content: string, versionNumber: number): Version {
     const lines = content.split('\n');
     for (const line of lines) {
         const hash = createHash(line);
-        version.hashes.push({ hashValue: hash });
+        version.hashes.push(new Hash(hash));
     }
     return version;
 }
