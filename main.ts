e79edc82312755d604612caa59ae1a1050ad5b1a
U-TELMA\nomena30 <nomena30@LTL253VT1F.corp.telma.mg>
2026-03-15 17:08:45 +0300

    test full

--- main.ts
+++ main.ts
@@ -29,13 +29,13 @@ class Version {
     versionNumber: number;
     createdAt: Date;
     updatedAt?: Date;
-    hashes: string[];
+    lines: number[];
 
     constructor(versionNumber: number, createdAt: Date, updatedAt?: Date) {
         this.versionNumber = versionNumber;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
-        this.hashes = [];
+        this.lines = [];
     }
     
 }
@@ -58,31 +58,52 @@ function createHash(hashlist: Hash[],hash: string, text: string): Hash {
     return hashlist[hashlist.length - 1];
 }
 
-function createVersion(content: string, versionNumber: number, hashes: Hash[]): Version {
-    const version = new Version(versionNumber, new Date());
-    const lines = content.split('\n');
-    for (const line of lines) {
-        const hash = getStringHash(line);
-        version.hashes.push(hashExists(hash, hashes)?.hashValue || createHash(hashes, hash, line).hashValue);
-    }
+function createVersion(content: string, versionNumber: number, versionList: Version[], versionLines: VersionLine[], hashes: Hash[]): Version {
+    const version = buildVersion(versionList, content.split('\n'), versionLines, hashes);
+    version.versionNumber = versionNumber;
     return version;
 }
 function hashExists(hash: string, hashList: Hash[]): Hash | null {
     return hashList.find(h => h.hashValue === hash) || null;
 }
 
-function buildVersion(versionList: Version[], content: string): Version {
+function buildVersion(versionList: Version[], lines: string[],versionLines: VersionLine[], hashList: Hash[]): Version {
     if (versionList.length === 0) {
-        const version = createVersion(content, 1, hashList);
+        const version = new Version(versionList.length+1, new Date());
+        
+        for (let i = 0; i < lines.length; i++) {
+            const hash = getStringHash(lines[i]);
+            const versionLine = new VersionLine(version, i + 1, hashExists(hash, hashList) || createHash(hashList, hash, lines[i]));
+            version.lines.push(versionLine.lineNumber);
+            versionLines.push(versionLine);
+        }
         versionList.push(version);
         return version;
     }
+    const version = new Version(versionList.length+1, new Date());
+    for (let i = 0; i < lines.length; i++) {
+            const hash = getStringHash(lines[i]);
+            const lastVersionNumber = versionList[versionList.length - 1].versionNumber;
+            const currentLine= i;
+            const lastVersionLine = versionLines.find(vl => vl.lineNumber === currentLine + 1 && vl.version.versionNumber === lastVersionNumber);
+            const lastHash = lastVersionLine?.hash.hashValue;
+            if (lastHash !== hash) {
+                const versionLine = new VersionLine(version, currentLine + 1, hashExists(hash, hashList) || createHash(hashList, hash, lines[i]));
+                version.lines.push(versionLine.lineNumber);
+                versionLines.push(versionLine);
+            }     
+    }
+    versionList.push(version);
+    return version;
+    
 }
 
 var hashList: Hash[] = [];
 var versionList: Version[] = [];
-const version1 = createVersion(content1, 1, hashList);
-const version2 = createVersion(content2, 2, hashList);
-console.log(version1.hashes);
-console.log(version2.hashes);
-console.log(hashList);
+const versionLines: VersionLine[] = [];
+const version1 = createVersion(content1, 1, versionList, versionLines, hashList);
+const version2 = createVersion(content2, 2, versionList, versionLines, hashList);
+
+console.log(version1 , "\n\n\n", version2, "\n\n\n", versionLines, "\n\n\n", hashList);
+
+
